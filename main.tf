e8758a72171e396244bd4fbb6f0cf06d8b4e823d
Jonathan Harden <jfharden@gmail.com>
2020-05-01 15:04:44 +0100

    Initial revision with only the org

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = var.org_service_access_principals 
+
+  enabled_policy_types = var.org_enabled_policy_types
+
+  feature_set = var.org_feature_set
+}
